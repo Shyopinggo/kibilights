b906d106386180e951e5a7d3735b3b7e713808b7
Vladimir Suhhorukov <vladimirsuhorukov@gmail.com>
2020-02-24 18:07:40 +0200

    feature(lighthouse): add create and remove facilities in admin area

--- Areas/Admin/Controllers/LighthouseController.cs
+++ Areas/Admin/Controllers/LighthouseController.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Authorization;
 using Microsoft.EntityFrameworkCore;
 
 using KibiLights.Models;
+using KibiLights.Models.Lighthouse;
 
 namespace KibiLights.Areas.Admin.Controllers
 {
@@ -20,11 +21,44 @@ namespace KibiLights.Areas.Admin.Controllers
         {
             this.context = context;
         }
-        
+
         public IActionResult Index()
         {
             var facilities = context.Facilities.Include(f => f.City).Include(f => f.City.Country).ToList();
             return View(facilities);
+        }
+
+        [HttpGet]
+        public IActionResult AddFacility()
+        {
+            ViewData["Cities"] = context.Cities.ToList();
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult AddFacility(string name, int cityId)
+        {
+            if (string.IsNullOrEmpty(name))
+            {
+                return Content("Name is empty!");
+            }
+            var city = context.Cities.FirstOrDefault(c => c.Id == cityId);
+            var facility = new Facility { Name = name, City = city};
+            context.Facilities.Add(facility);
+            context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult DeleteFacility(int id)
+        {
+            Facility facility = context.Facilities.FirstOrDefault(f => f.Id == id);
+            if (facility != null)
+            {
+                context.Facilities.Remove(facility);
+                context.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return Content("Facility id was null");
         }
     }
 }
