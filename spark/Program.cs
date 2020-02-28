a798f0fb30589df82030debb150b99387a015668
Vladimir Suhhorukov <vladimirsuhorukov@gmail.com>
2020-02-28 11:14:03 +0200

    chore(kibilights): add spark project

---
+++ spark/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Spark
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
